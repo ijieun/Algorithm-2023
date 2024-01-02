12a16385b16af823795adf5ddd221c38116d3280
Jieun Lee <99737532+ijieun@users.noreply.github.com>
2024-01-03 01:31:05 +0900

    [Silver IV] Title: 동전 0, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+n, k =map(int, input().split())
+li=[]
+for i in range(n):
+    a= int(input())
+    li.append(a)
+li.sort(reverse=True)
+cnt =0
+for j in li:
+    if j<=k:
+        cnt+=k//j
+        k -= (k//j)*j
+print(cnt)        

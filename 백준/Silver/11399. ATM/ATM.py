735d21aa1af3f481ef68c2bf81ab04b505be2128
Jieun Lee <99737532+ijieun@users.noreply.github.com>
2024-01-02 23:35:17 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -4,8 +4,8 @@ n = int(input())
 li = list(map(int, input().split()))
 sum = 0
 result=0
-sorted_li = sorted(li)
-for i in sorted_li:
+li.sort()
+for i in li:
     sum+=i
     result += sum
 print(result)

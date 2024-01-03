7cf9b43ff38f93ffd189b5e0545e8c8d2203a973
Jieun Lee <99737532+ijieun@users.noreply.github.com>
2024-01-03 12:03:35 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 46228 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -5,9 +5,11 @@ dis=list(map(int, input().split()))
 cost=list(map(int, input().split()))
 result = 0
 
+min_cost = cost[0]
 for i in range(n-1):
-    if cost[i]>cost[i+1]:
-        result += cost[i]*dis[i]
+    if cost[i]<min_cost:
+        min_cost=cost[i]
+        result += min_cost*dis[i]
     else:
-        result += cost[i]*dis[i-1]
+        result += min_cost*dis[i]
 print(result)

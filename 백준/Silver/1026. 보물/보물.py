0362be4c6cddc9d8b8a7bd8bda398db02a9aaa61
Jieun Lee <99737532+ijieun@users.noreply.github.com>
2024-01-03 10:52:08 +0900

    [Silver IV] Title: 보물, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+a=list(map(int, input().split()))
+b=list(map(int, input().split()))
+sum = 0
+a.sort()
+b.sort(reverse=True)
+for i in range(n):
+    sum += a[i]*b[i]
+print(sum)

bd5dfab2567f08da0c5b044c1ace20d0a3c6bd17
Jieun Lee <99737532+ijieun@users.noreply.github.com>
2024-01-14 18:18:34 +0900

    [Silver IV] Title: 듣보잡, Time: 88 ms, Memory: 43000 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+N = []
+M = []
+answer = []
+hashmap = {}
+
+n,m = map(int, input().split())
+for i in range(n):
+    N.append(input())
+for i in range(m):
+    M.append(input())
+    
+for i in N:
+    if i in hashmap:
+        hashmap[i] += 1
+    else:
+        hashmap[i] = 1
+for i in M:
+    if i in hashmap:
+        answer.append(i)
+print(len(answer))
+for i in sorted(answer):
+    print(i, end='')
